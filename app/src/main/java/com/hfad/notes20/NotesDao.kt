1b66d7dfc8398105f901e5ee4dbf6b5915aaf9cd
AlenNka333 <elena.walst@bk.ru>
2020-06-09 23:29:21 +0300

    rewrite using database to Room

---
+++ app/src/main/java/com/hfad/notes20/NotesDao.kt
@@ -0,0 +1,22 @@
+package com.hfad.notes20
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface NotesDao {
+
+    @Query("SELECT * FROM notes ORDER BY day")
+    fun getAllNotes():List<Note>
+
+    @Insert
+    fun insertNote(note:Note)
+
+    @Delete
+    fun deleteNote(note:Note)
+
+    @Query("DELETE FROM notes")
+    fun deleteAllNotes()
+}
---
+++ app/src/main/java/com/hfad/notes20/NotesDatabase.kt
@@ -0,0 +1,30 @@
+package com.hfad.notes20
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import java.util.*
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NotesDatabase: RoomDatabase() {
+
+    companion object {
+        var database: NotesDatabase? = null
+        val DB_NAME = "notes1.db"
+        val LOCK = Object()
+
+
+        fun getInstance(context: Context): NotesDatabase?{
+            if (database == null) {
+                synchronized(LOCK) {
+                    database = Room.databaseBuilder(context, NotesDatabase::class.java, DB_NAME)
+                        .build()
+                }
+            }
+            return database
+        }
+    }
+
+    abstract fun notesDao(): NotesDao
+}
